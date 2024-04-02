e261f3f8bbb771b4395d9b3406200fe0d052bfa7
martinoremonje <m.monjeorellana@gmail.com>
2024-04-02 12:02:42 -0300

    subida 2

--- assets/index.js
+++ assets/index.js
@@ -11,10 +11,11 @@ const obtenerPosts = async () => {
         const data = await response.json();
         data.forEach(e=>{
             if(e.id <= 6){
-                box.style.border = "2px solid black"
+                
                 list.innerHTML += `<li><h4>${e.title}</h4>${e.body}</li>`
             }
         }) 
+        box.style.border = "2px solid black";
     } catch (err) {
         console.log(err);
     }
