11241235f50f458490996e0bad4dcaefa9d0a6a0
martinoremonje <m.monjeorellana@gmail.com>
2024-04-02 11:56:49 -0300

    subida de desafio evaluado async await

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+document.getElementById("btn").addEventListener("click", ()=>{
+    obtenerPosts();
+});
+
+const obtenerPosts = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    let list = document.getElementById("list");
+    let box = document.getElementById("box")
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        data.forEach(e=>{
+            if(e.id <= 6){
+                box.style.border = "2px solid black"
+                list.innerHTML += `<li><h4>${e.title}</h4>${e.body}</li>`
+            }
+        }) 
+    } catch (err) {
+        console.log(err);
+    }
+};
