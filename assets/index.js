3c08170abaa2e343c1aba4f6998004ccfa633e3e
martinoremonje <m.monjeorellana@gmail.com>
2024-04-02 20:31:46 -0300

    sin limite api

--- assets/index.js
+++ assets/index.js
@@ -10,10 +10,10 @@ const obtenerPosts = async () => {
         const response = await fetch(url);
         const data = await response.json();
         data.forEach(e=>{
-            if(e.id <= 6){
+         
                 
                 list.innerHTML += `<li><h4>${e.title}</h4>${e.body}</li>`
-            }
+          
         }) 
         box.style.border = "2px solid black";
     } catch (err) {
